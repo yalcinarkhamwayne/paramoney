41544b8f000268632784d3700c87d8853bffb935
yalcinarkhamwayne <yalcinarkhamwayne@gmail.com>
2025-02-07 17:58:03 +0100

    main geschrieben

---
+++ main.py
@@ -0,0 +1,19 @@
+# ~~~~~~~~~~ ParaMoney ~~~~~~~~~~~~~~~~~
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+class MainScreen(BoxLayout):
+    def __init__(self, **kwargs):
+        super().__init__(orientation="vertical", **kwargs)
+        self.label = Label(text="Willkommen zu deiner Finanz-App!")
+        self.add_widget(self.label)
+        self.add_widget(Button(text="Neue Einnahme/Ausgabe hinzufügen"))
+
+class MyApp(App):
+    def build(self):
+        return MainScreen()
+
+if __name__ == "__main__":
+    MyApp().run()
